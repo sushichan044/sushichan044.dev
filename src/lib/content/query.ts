282c9d18a63c2059d0fed321ceb1bffb86511ea1
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2026-06-28 23:22:18 +0900

    fix

--- src/lib/content/query.ts
+++ src/lib/content/query.ts
@@ -6,10 +6,7 @@ export async function getAllPublicPosts() {
 
 export async function getRecentPublicPosts(limit: number) {
   const data = (await getAllPublicPosts()).sort((a, b) => {
-    const aData = a.data.updatedAt ?? a.data.publishedAt;
-    const bData = b.data.updatedAt ?? b.data.publishedAt;
-
-    return Temporal.Instant.compare(bData, aData);
+    return Temporal.Instant.compare(b.data.publishedAt, a.data.publishedAt);
   });
 
   return data.slice(0, limit);
