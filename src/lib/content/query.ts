7aa0ae9e7651eb922f61e68a8e0b5d7d2fe02a3d
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2026-07-10 02:18:01 +0900

    廃墟めぐり1

--- src/lib/content/query.ts
+++ src/lib/content/query.ts
@@ -5,9 +5,26 @@ export async function getAllPublicPosts() {
 }
 
 export async function getRecentPublicPosts(limit: number) {
-  const data = (await getAllPublicPosts()).sort((a, b) => {
-    return Temporal.Instant.compare(b.data.publishedAt, a.data.publishedAt);
-  });
+  const data = sortPostsByPublishedAt(await getAllPublicPosts(), "desc");
 
   return data.slice(0, limit);
+}
+export function sortPostsByPublishedAt(
+  posts: Awaited<ReturnType<typeof getAllPublicPosts>>,
+  order: "asc" | "desc" = "desc",
+) {
+  return posts.sort((a, b) => {
+    if (order === "asc") {
+      return Temporal.Instant.compare(a.data.publishedAt, b.data.publishedAt);
+    } else {
+      return Temporal.Instant.compare(b.data.publishedAt, a.data.publishedAt);
+    }
+  });
+}
+
+export function filterPostsByTags(
+  posts: Awaited<ReturnType<typeof getAllPublicPosts>>,
+  tags: string[],
+) {
+  return posts.filter((post) => tags.every((tag) => post.data.tags.includes(tag)));
 }
